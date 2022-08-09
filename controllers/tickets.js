f724eb236c8fd67f807889a720e8d0f8f437fe66
ngran4 <nicgranville@gmail.com>
2022-08-08 17:33:40 -0700

    ticket form

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -2,7 +2,8 @@ const Ticket = require('../models/ticket');
 const Flight = require('../models/flight');
 
 module.exports = {
-    new: newTicket
+    new: newTicket,
+    create
 }
 
 
@@ -11,5 +12,14 @@ function newTicket(req, res){
 };
 
 function create(req, res){
-    
+    console.log(req.params.id, '<- params flight id');
+    console.log(req.body, 'contents of form (ticket)');
+
+    Flight.findById(req.params.id, function(err, flightDoc){
+        console.log(flightDoc, '<- flightDoc');
+        flightDoc.tickets.push(req.body);
+        flightDoc.save(function(err) {
+            res.redirect(`flights/${req.params.id}`)
+        })
+    })
 }
